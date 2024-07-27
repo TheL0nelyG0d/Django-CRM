d4cc625fa8747e81da7901a359743440cd08d962
TheL0nelyG0d <cetoerdem1@gmail.com>
2024-07-27 20:40:16 +0300

    change db pass

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dcrm',
         'USER': 'root',
-        'PASSWORD': 'Arzumkuzum2134-',
+        'PASSWORD': '***********',
         'HOST': 'localhost',
         'PORT': '3306',
     }
